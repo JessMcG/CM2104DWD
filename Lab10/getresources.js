4f3be7cc09ea08397c2463ac5835dc38fdb4e157
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-03-08 09:36:04 +0000

    minor changes

--- Lab10/getresources.js
+++ Lab10/getresources.js
@@ -11,10 +11,10 @@ $(document).ready(function () {
         var output="<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";
         for (var i in result) {
           output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
-          output+="</tbody></table>";
-          displayResources.html(output);
-          $("table").addClass("table");
         }
+        output+="</tbody></table>";
+        displayResources.html(output);
+        $("table").addClass("table");
       }
     });
   });
