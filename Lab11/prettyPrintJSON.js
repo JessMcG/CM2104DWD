2d8918a5abef5738cecd1909474777eae530a37d
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-03-08 10:53:15 +0000

    Pretty JSON

---
+++ Lab11/prettyPrintJSON.js
@@ -0,0 +1,26 @@
+$(function(){
+  alert("document ready");
+
+  $('#searchform').submit(function(){
+    var searchterms  = $("#searchterms").val();
+    //call the search OMDB function
+    getResultsFromOMDB(searchterms);
+    return false;
+  });
+
+});
+
+function getResultsFromOMDB(searchterms) {
+  var url = "http://www.omdbapi.com/?apikey=11b88edf&s=" + searchterms;
+  //use jQuery JSON shortcut
+  $.getJSON(url, function(jsondata){
+    //handle the results
+    printJSON(jsondata);
+  });
+}
+
+function prettyPrintJSON (jsondata){
+  //prints JSON to screen
+  var pretty = JSON.stringify(jsondata, null, 4);
+  $('#resultsbox').append("<p>"+pretty+"</p>");
+}
