53e8b115ceef9c3c37ae893e0fec6b11cc8b1e04
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-03-12 16:34:33 +0000

    added /add

--- Lab13/server.js
+++ Lab13/server.js
@@ -16,4 +16,11 @@ app.get('/joke', function(req, res){
   res.send(randomJoke.body);
 });
 
+app.get('/add', function(req, res){
+  var x = parseInt(req.query.x);
+  var y = parseInt(req.query.y);
+  res.send("X + Y="+(x+y));
+});
+
+
 app.listen(8080);
