4e9e057135b69e8b86cd8ef365d0286a1af6a564
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-03-12 16:25:27 +0000

    Added /joke

--- Lab13/server.js
+++ Lab13/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 
+var oneLinerJoke = require('one-liner-joke');
+
 app.get('/', function(req, res){
   res.send("Hello world! by express");
 });
 
 app.get('/test', function(req, res){
   res.send("this is route 2");
-})
+});
+
+app.get('/joke', function(req, res){
+  var randomJoke = oneLinerJoke.getRandomJoke();
+  res.send(randomJoke);
+});
 
 app.listen(8080);
