fce4384b5cf86dcc05806b0f2d6896e52962c3ea
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-03-19 15:19:50 +0000

    Added 'add' page

--- Lab15/server.js
+++ Lab15/server.js
@@ -1,9 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
 
 var db;
 
@@ -25,5 +27,13 @@ app.get('/all', function(req, res) {
       output += "</div>";
     }
     res.send(output);
+  });
+});
+
+app.post('/quotes', function (req, res) {
+  db.collection('quotes').save(req.body, function(err, result) {
+    if (err) throw err;
+    console.log('Saved to Database');
+    res.redirect('/');
   });
 });
