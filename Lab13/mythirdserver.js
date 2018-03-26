08a7ffb9f9099a08165832e1cdcea376817f0c5f
JESS MCGOWAN (1703725) <1703725@rgu.ac.uk>
2018-03-26 12:35:29 +0100

    Updated Joke Package

--- Lab13/mythirdserver.js
+++ Lab13/mythirdserver.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var oneLinerJoke = require('one-liner-joke');
+var knockKnock = require('knock-knock-jokes');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var randomJoke = oneLinerJoke.getRandomJoke();
+  var randomJoke = knockKnock.getRandomJoke();
   res.end(randomJoke.body);
 }).listen(8080);
